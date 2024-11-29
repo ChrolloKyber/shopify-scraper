3a1eadda40f1eb472561b1ded58f244e84f0dde7
ChrolloKryber <rishay.raghav00@gmail.com>
2024-11-29 21:08:27 +0530

    atleast the downloads are fast now
    Signed-off-by: ChrolloKryber <rishay.raghav00@gmail.com>

--- scraper.go
+++ scraper.go
@@ -25,36 +25,41 @@ func DownloadJSON() {
 
 	baseURL := "https://"
 	product := "/products.json"
+	var wg sync.WaitGroup
 
-	// TODO: implement go routines
 	for k, v := range data {
 		if k == 0 {
 			continue
 		}
 
 		fmt.Println(k, v[0], v[1])
+		wg.Add(1)
 
-		file, err := http.Get(baseURL + v[1] + product)
-		if err != nil {
-			fmt.Println(err)
-		}
-		defer file.Body.Close()
+		go func(v []string) {
+			file, err := http.Get(baseURL + v[1] + product)
+			if err != nil {
+				fmt.Println(err)
+			}
+			defer file.Body.Close()
 
-		out, err := os.Create("json/" + v[0] + ".json")
-		if err != nil {
-			fmt.Println(err)
-		}
-		defer out.Close()
+			out, err := os.Create("json/" + v[0] + ".json")
+			if err != nil {
+				fmt.Println(err)
+			}
+			defer out.Close()
 
-		if file.StatusCode != http.StatusOK {
-			fmt.Printf("Error writing: %s", file.Status)
-		}
+			if file.StatusCode != http.StatusOK {
+				fmt.Printf("Error writing: %s", file.Status)
+			}
 
-		_, err = io.Copy(out, file.Body)
-		if err != nil {
-			fmt.Printf("Error writing to the file: %s", err)
-		}
+			_, err = io.Copy(out, file.Body)
+			if err != nil {
+				fmt.Printf("Error writing to the file: %s", err)
+			}
+			defer wg.Done()
+		}(v)
 	}
+	wg.Wait()
 	fmt.Println("All tasks completed.")
 }
 
