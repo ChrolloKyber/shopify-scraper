d6e08b4d771d40c09da0d5143fb50d887d9c90b6
Chatheriyan Thangaraju <chatheriyan.thangaraju@qburst.com>
2025-02-04 15:23:51 +0530

    LImiter with IP included
    Signed-off-by: Chatheriyan Thangaraju <chatheriyan.thangaraju@qburst.com>

--- limiter/limiter.go
+++ limiter/limiter.go
@@ -2,16 +2,53 @@ package limiter
 
 import (
 	"encoding/json"
+	"net"
 	"net/http"
+	"sync"
+	"time"
 
 	"github.com/ChrolloKryber/shopify-scraper/models"
 	"golang.org/x/time/rate"
 )
 
-func RateLimiter(next func(w http.ResponseWriter, r *http.Request)) http.Handler {
-	limiter := rate.NewLimiter(2, 4)
+func PerClientRateLimiter(next func(w http.ResponseWriter, r *http.Request)) http.Handler {
+
+	type Client struct {
+		limiter  *rate.Limiter
+		lastSeen time.Time
+	}
+	var (
+		mu      sync.Mutex
+		clients = make(map[string]*Client)
+	)
+
+	go func() {
+		for {
+			time.Sleep(time.Minute)
+			mu.Lock()
+			for ip, client := range clients {
+				if time.Since(client.lastSeen) > 3*time.Minute {
+					delete(clients, ip)
+				}
+			}
+			mu.Unlock()
+		}
+	}()
+
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if !limiter.Allow() {
+		ip, _, err := net.SplitHostPort(r.RemoteAddr)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		mu.Lock()
+		if _, found := clients[ip]; !found {
+			clients[ip] = &Client{limiter: rate.NewLimiter(2, 4)}
+		}
+		clients[ip].lastSeen = time.Now()
+		if !clients[ip].limiter.Allow() {
+			mu.Unlock()
+
 			msg := models.Message{
 				Status: "Request failed",
 				Body:   "API max capacity reached. Try again!",
@@ -19,8 +56,9 @@ func RateLimiter(next func(w http.ResponseWriter, r *http.Request)) http.Handler
 			w.WriteHeader(http.StatusTooManyRequests)
 			json.NewEncoder(w).Encode(&msg)
 			return
-		} else {
-			next(w, r)
 		}
+		mu.Unlock()
+		next(w, r)
+
 	})
 }
